37b939d1d5379589561561fea3e020094fd1f2e3
Anna <anna.yanova@gmail.com>
2017-12-16 23:06:43 +0000

    Added tests for multiples of five and multiples of both three and five

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,11 @@
-
-def fizz_buzz(n)
-  'Fizz'
+def fizz_buzz(number)
+    if number % 15 == 0
+     'FizzBuzz'
+    elsif number % 3 == 0
+     'Fizz'
+    elsif number % 5 == 0
+     'Buzz'
+    else
+      number
+    end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,8 +2,28 @@ require 'fizz_buzz'
 
 describe 'FizzBuzz' do
 
-  it "prints 'Fizz' for 3" do
-    expect(fizz_buzz(3)).to eq 'Fizz'
-  end
+    it "prints 'Fizz' for the number 3" do
+      expect(fizz_buzz(3)).to eq 'Fizz'
+    end
+
+    it "prints 'Buzz' for the number 5" do
+      expect(fizz_buzz(5)).to eq 'Buzz'
+    end
+
+    it "prints 'FizzBuzz' for the number 15" do
+      expect(fizz_buzz(15)).to eq 'FizzBuzz'
+    end
+
+    it "prints 'FizzBuzz' for the number 45" do
+      expect(fizz_buzz(45)).to eq 'FizzBuzz'
+    end
+
+    it "not divisible by 3 and 5" do
+      expect(fizz_buzz(2)).to eq 2
+    end
+
+    it "not divisible by 3 and 5" do
+      expect(fizz_buzz(16)).to eq 16
+    end
 
 end
