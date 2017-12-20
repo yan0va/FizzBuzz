a36ed0684c61a2e31e818e932abf53fcd26c56a1
Anna <anna.yanova@gmail.com>
2017-12-20 20:49:38 +0000

    Created api with sinatra and changed the appearance through css file

--- app.rb
+++ app.rb
@@ -1,2 +1,15 @@
 require 'sinatra'
-require 'fizz_buzz.rb'
+require './lib/fizz_buzz.rb'
+
+get '/' do
+  if params[:number]
+    @number = params[:number].to_i
+    if @number > 100
+       @limit_exceeded = true
+       @number = 100
+    end
+    @result = fizz_buzz(@number)
+    @flamingo = @result == 'Flamingo'
+  end
+  erb :index
+end
